ac15910e70a7d9961e54eef7ff7ef55f4ffb4dbd
DoroteaSavariso98 <45691882+DoroteaSavariso98@users.noreply.github.com>
2019-01-13 11:16:44 +0100

    Add files via upload

---
+++ SPLC.py
@@ -0,0 +1,47 @@
+genetic_code = {
+    'TTT': 'F',     'CTT': 'L',     'ATT': 'I',     'GTT': 'V',
+    'TTC': 'F',     'CTC': 'L',     'ATC': 'I',     'GTC': 'V',
+    'TTA': 'L',     'CTA': 'L',     'ATA': 'I',     'GTA': 'V',
+    'TTG': 'L',     'CTG': 'L',     'ATG': 'M',     'GTG': 'V',
+    'TCT': 'S',     'CCT': 'P',     'ACT': 'T',     'GCT': 'A',
+    'TCC': 'S',     'CCC': 'P',     'ACC': 'T',     'GCC': 'A',
+    'TCA': 'S',     'CCA': 'P',     'ACA': 'T',     'GCA': 'A',
+    'TCG': 'S',     'CCG': 'P',     'ACG': 'T',     'GCG': 'A',
+    'TAT': 'Y',     'CAT': 'H',     'AAT': 'N',     'GAT': 'D',
+    'TAC': 'Y',     'CAC': 'H',     'AAC': 'N',     'GAC': 'D',
+    'TAA': 'Stop',  'CAA': 'Q',     'AAA': 'K',     'GAA': 'E',
+    'TAG': 'Stop',  'CAG': 'Q',     'AAG': 'K',     'GAG': 'E',
+    'TGT': 'C',     'CGT': 'R',     'AGT': 'S',     'GGT': 'G',
+    'TGC': 'C',     'CGC': 'R',     'AGC': 'S',     'GGC': 'G',
+    'TGA': 'Stop',  'CGA': 'R',     'AGA': 'R',     'GGA': 'G',
+    'TGG': 'W',     'CGG': 'R',     'AGG': 'R',     'GGG': 'G'
+}
+
+
+def aaseq(s):
+    result = ''
+    dna = s[0]
+    introns = s[1:]
+    for intron in introns:
+        dna = dna.replace(intron, '')
+    for i in range(0, len(dna), 3):
+        codon = dna[i:i+3]
+        protein = None
+        if codon in genetic_code:
+            protein = genetic_code[codon]
+        if protein == 'Stop':
+            break
+        if protein != 'Stop':
+            result += protein
+    return ''.join(list(result))
+
+f = open("rosalind_splc.txt", "r")
+data = []
+str1 = f.read()
+str1 = str1.replace("Rosalind_", "")
+str1 = str1.replace("\n", "")
+str1 = ''.join([i for i in str1 if not i.isdigit()])
+data = str1.split(">")
+data.remove("")
+
+print(aaseq(data))
